b4296a8fdb218d12c604fd2d49cef3122eb9c4fb
Mike Piehl <piehlm@paperstreetenterprises.com>
2021-08-05 08:01:41 -0400

    Updates for Kickstarter

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -17,4 +17,81 @@ beforeEach(async () => {
 	factory = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
 		.deploy({ data: compiledFactory.bytecode })
 		.send({ from: accounts[0], gas: 1000000 });
+
+	await factory.methods.createCampaign('100').send({ 
+		from: accounts[0],
+		gas: '1000000'
+	});
+
+	[campaignAddress] = await factory.methods.getDeployedCampaigns().call();
+	campaign = await new web3.eth.Contract(
+		JSON.parse(compiledCampaign.interface),
+		campaignAddress
+	);
+});
+
+describe('Campaigns', () => {
+	it('deploys a factory and a campaign', () => {
+		assert.ok(factory.options.address, 'Factory Address is missing');
+		assert.ok(campaign.options.address, 'Campaign Address is missing');
+	});
+
+  	it('marks caller as the campaign manager', async () => {
+    	const manager = await campaign.methods.manager().call();
+		assert.equal(accounts[0], manager, 'Manager was not set properly');
+	});
+
+	it('allows people to contribute money and marks them as approvers', async () => {
+		await campaign.methods.contribute().send({
+			value: '200',
+			from: accounts[1]
+		});
+		const isContributor = await campaign.methods.approvers(accounts[1]).call();
+		assert(isContributor, 'Contributor is not in the approvers list');
+	});
+
+	it('requires a minimum contribution', async () => {
+		try {
+			await campaign.methods.contribute().send({
+				value: '5',
+				from: accounts[1]
+			});
+			assert(false);
+		} catch (err) {
+			assert(err);
+		}
+	});
+
+	it('allows a manager to make a payment request', async () => {
+		await campaign.methods.createRequest('Buy Batteries', '100', accounts[9]).send({
+			from: accounts[0],
+			gas: '1000000'
+		});
+		const request = await campaign.methods.requests(0).call();
+		assert.equal('Buy Batteries', request.description);
+	});
+
+	it('processes requests', async () => {
+		await campaign.methods.contribute().send({
+			value: web3.utils.toWei('10', 'ether'),
+			from: accounts[2]
+		});
+		await campaign.methods.createRequest('A', web3.utils.toWei('5', 'ether'), accounts[9]).send({
+			from: accounts[0],
+			gas: '1000000'
+		});
+		await campaign.methods.approveRequest(0).send({
+			from: accounts[2],
+			gas: '1000000'
+		});
+		await campaign.methods.finalizeRequest(0).send({
+			from: accounts[0],
+			gas: '1000000'			
+		});
+		let balance = await web3.eth.getBalance(accounts[9]);
+		balance = web3.utils.fromWei(balance, 'ether');
+		balance = parseFloat(balance);
+
+		assert(balance > 104);
+	});
 });
