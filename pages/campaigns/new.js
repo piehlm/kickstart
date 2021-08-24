5ddaf5dcc7da8bcab098594004b84c6a32302066
Mike Piehl <piehlm@paperstreetenterprises.com>
2021-08-24 09:09:09 -0400

    route determination

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -3,6 +3,7 @@ import { Form, Input, Button, Message } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
 import factory from '../../ethereum/factory';
 import web3 from '../../ethereum/web3';
+import { Router } from '../../routes';
 
 class CampaignNew extends Component {
 	state = {
@@ -22,7 +23,9 @@ class CampaignNew extends Component {
 				.createCampaign(this.state.minimumContribution)
 				.send({
 					from: accounts[0]
-				});			
+				});
+
+			Router.pushRoute('/');
 		} catch (err) {
 			this.setState({ errorMessage: err.message });
 		}
