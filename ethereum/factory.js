607da668f7a963c2a78aa32f51a82d41a2fb7604
Mike Piehl <piehlm@paperstreetenterprises.com>
2021-08-31 12:19:49 -0400

    Updated Campaign contract
    added multiple new pages

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
 	JSON.parse(CampaignFactory.interface),
-	'0xc8B125990c9e8E22068eFd4826afdf407eE99E00'
+	'0x40CC6193Aa496CFde518f66240A13D54b4e3F5E9'
 );
 
 export default instance;
